ac7408d1e6ac0c545bd25dcfa39cca708f0d2dfa
mtalarczyk <michal.talarczyk@yahoo.com>
2016-11-11 14:52:28 +0100

    Add sticky menu, window scrolling, gallery with zoom and categories

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
-    
-    
-    console.log("Plik JS prawidłowo załadować!");
+$(function(){
     
 //    var nav = document.querySelector('nav');
 //    var button = document.querySelector('button');
@@ -20,5 +17,79 @@ document.addEventListener("DOMContentLoaded", function(){
 //        
 //        
 //    });
- 
+    
+    
+    
+//Scroll menu
+    var menu = $('#header, #navigation');
+    var nav = $('nav');
+    //$.waypoints.settings.scrollThrottle=0;
+    
+    menu.on('click', 'a', function(){
+        
+        var href = $(this).attr('href');
+        var offsetNav = $(href).offset().top-nav.height();
+                        
+        $('html, body').animate({   
+                    scrollTop: offsetNav
+        }, 1000);
+        
+    });
+
+//Sticky menu
+    var navTop = $('#navigation');
+//    var navTop = $('#navigation');
+//    var top = navTop.position().top;
+    
+    $(window).on('scroll resize', function(){
+        var top = navTop.position().top;
+        var distanceTop = $(document).scrollTop();
+//        top = navTop.position().top;
+        
+        (distanceTop > top) ? nav.addClass('sticky') : nav.removeClass('sticky'); 
+    });
+    
+//    $(window).on('resize', function(){
+//        var distanceTop = $(document).scrollTop(); 
+//        top = navTop.position().top;
+
+//        (distanceTop > top) ? nav.addClass('sticky') : nav.removeClass('sticky');
+//        
+//    });
+    
+ //Portfolio - Zoom
+    var list = $('.gallery').find('[class^="img-"]');
+    var body = $('body');
+        
+    var div = $("<div>").addClass('fullScreen').html('<img title="Click to close!" />');
+    var imgZoom = div.find('img');
+    
+    list.on('click', function(){
+    
+        imgZoom.attr('src', "../images/portfolio/0"+$(this).attr('data-id')+".png");
+        div.appendTo(body);
+    });
+    
+    imgZoom.on('click', function(){
+        div.detach();
+    });
+
+//Portolio - filter
+    
+    var btns = $('.categories').find('div');
+    var lablelList = list.siblings('p');
+    btns.eq(0).addClass('checked');
+    
+    btns.on('click', function(){
+        var btnText = $(this).text().toLowerCase();
+        btns.removeClass('checked');
+        $(this).addClass('checked');
+
+        lablelList.each(function(){
+           ($(this).text().search(btnText) != -1) ? $(this).parent().removeClass('invisible') : $(this).parent().addClass('invisible');
+        });
+    });
+    
+    
+    
 });   
